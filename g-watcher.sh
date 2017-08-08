82e824b291c3c3e1ef889b48b5e2a68020f6954d
Ioannis Gasparis <igasparis@nowsecure.com>
2017-08-07 22:53:39 -0500

    Added script

--- g-watcher.sh
+++ g-watcher.sh
@@ -11,49 +11,49 @@ BINARY=""
 MAKE=""
 
 while getopts "m::hb:d:" opt; do
-	case $opt in
-		m)
-			MAKE=${OPTARG}
-			;;
-		b)
-			BINARY=${OPTARG}
-			;;
-		d)
-			DIR=${OPTARG}
-			;;
-		h)
-			usage
-			;;
-		*)
-			usage
-			;;
-		esac
+  case $opt in
+    m)
+      MAKE=${OPTARG}
+      ;;
+    b)
+      BINARY=${OPTARG}
+      ;;
+    d)
+      DIR=${OPTARG}
+      ;;
+    h)
+      usage
+      ;;
+    *)
+      usage
+    ;;
+  esac
 done
 
 if [ -z "${DIR}" ] || [ -z "${BINARY}" ] || [ -z "${MAKE}" ]; then
-    usage
+  usage
 fi
 
 echo "Watching directory ${DIR}..."
 inotifywait --exclude '.*\.swp|.*\.o|.*~' --event MODIFY -q -m -r ${DIR} | while read 
 do 
-	if (( $SKIP > 0 )); then
-		SKIP=1
-		continue;
-	fi
+  if (( $SKIP > 0 )); then
+    SKIP=1
+    continue; 
+  fi
 	
-	make -j8 -C ${MAKE}
-	if (( $? > 0 )); then
-		notify-send 'Build failed'
-		continue
-	fi
-	echo "Build succeeded"
-	echo "Running ${BINARY}"
-	$BINARY
-	if (( $? > 0 )); then
-		notify-send 'Test failed'
-		continue
-	fi
+  make -j8 -C ${MAKE}
+  if (( $? > 0 )); then
+    notify-send 'Build failed'
+    continue
+  fi
+  echo "Build succeeded"
+  echo "Running ${BINARY}"
+  $BINARY
+  if (( $? > 0 )); then
+    notify-send 'Test failed'
+    continue
+  fi
 	echo "Test succeeded"
 	
 	SKIP=0
